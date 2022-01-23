0050bd820bbee89bd102e2613ea207261d2355a5
Kelocy <ceolhx@gmail.com>
2022-01-23 14:56:52 +0900

    implement address service

---
+++ src/main/java/com/cy/store/service/impl/AddressServiceImpl.java
@@ -0,0 +1,47 @@
+package com.cy.store.service.impl;
+
+import com.cy.store.entity.Address;
+import com.cy.store.mapper.AddressMapper;
+import com.cy.store.service.IAddressService;
+import com.cy.store.service.ex.AddressCountLimitException;
+import com.cy.store.service.ex.InsertException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/** 表示新增收货地址的实现类 */
+@Service
+public class AddressServiceImpl implements IAddressService {
+    @Autowired
+    private AddressMapper addressMapper;
+
+    @Value("${user.address.max-count}")
+    private Integer maxCount;
+
+    @Override
+    public void addNewAddress(Integer uid, String username, Address address) {
+        // 调用收货地址统计的方法
+        Integer count = addressMapper.countByUid(uid);
+        if (count >= maxCount) {
+            throw new AddressCountLimitException("用户收货地址超出上限");
+        }
+
+        // uid, isDefault
+        address.setUid(uid);
+        Integer isDefault = count == 0 ? 1 : 0; // 1表示默认，0表示不是默认
+        address.setIsDefault(isDefault);
+        // 补全4项日志
+        address.setCreatedUser(username);
+        address.setModifiedUser(username);
+        address.setCreatedTime(new Date());
+        address.setModifiedTime(new Date());
+
+        // 插入收货地址的方法
+        Integer rows = addressMapper.insert(address);
+        if (rows != 1) {
+            throw new InsertException("插入用户的收货地址产生未知的异常");
+        }
+    }
+}
---
+++ src/test/java/com/cy/store/service/AddressServiceTests.java
@@ -0,0 +1,32 @@
+package com.cy.store.service;
+
+
+import com.cy.store.entity.Address;
+import com.cy.store.entity.User;
+import com.cy.store.mapper.UserMapper;
+import com.cy.store.service.ex.ServiceException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+// @SpringBootTest：表示标注当前的类是一个测试类，不会随项目一块打包
+@SpringBootTest
+// @RunWith: 表示启动这个单元测试类(单元测试类是不能够运行的)， 需要传递一个参数，必须是SpringRunner的实例类型
+@RunWith(SpringRunner.class)
+public class AddressServiceTests {
+    // idea有检测功能，接口是不能直接创建Bean的(动态代理技术解决)
+    @Autowired
+    private IAddressService addressService;
+
+    @Test
+    public void addNewAddress() {
+        Address address = new Address();
+        address.setPhone("323447344325");
+        address.setName("boyfriend");
+        addressService.addNewAddress(9, "Administrator", address);
+    }
+
+
+}
