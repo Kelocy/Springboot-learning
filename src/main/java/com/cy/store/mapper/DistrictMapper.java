115924a587b80ce8eb6c3e12055bf5df373e22f5
Kelocy <ceolhx@gmail.com>
2022-01-23 17:04:12 +0900

    implement district list DAO

---
+++ src/main/java/com/cy/store/mapper/DistrictMapper.java
@@ -0,0 +1,16 @@
+package com.cy.store.mapper;
+
+import com.cy.store.entity.District;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DistrictMapper {
+    /**
+     * 根据用户的父代号查询区域信息
+     * @param parent    父代号
+     * @return  某个父区域下的所有区域列表
+     */
+    List<District> findByParent(String parent);
+}
---
+++ src/test/java/com/cy/store/mapper/DistrictMapperTests.java
@@ -0,0 +1,28 @@
+package com.cy.store.mapper;
+
+
+import com.cy.store.entity.Address;
+import com.cy.store.entity.District;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DistrictMapperTests {
+    // idea有检测功能，接口是不能直接创建Bean的(动态代理技术解决)
+    @Autowired
+    private DistrictMapper districtMapper;
+
+    @Test
+    public void findByParent() {
+        List<District> list = districtMapper.findByParent("210100");
+        for (District d: list) {
+            System.out.println(d);
+        }
+    }
+}
